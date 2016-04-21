afc526ac9044f7ca53626b9a021da50d838b178d
SGrottel-TUD <sebastian.grottel@tu-dresden.de>
2016-04-21 14:19:03 +0200

    debug_renderer output code written.

--- src/rendering/debug_renderer.cpp
+++ src/rendering/debug_renderer.cpp
@@ -1,9 +1,45 @@
 #include "debug_renderer.hpp"
+#include "data/world.hpp"
 #include "GL/glew.h"
+#include <iostream>
 
 using namespace cgvkp;
 
-rendering::debug_renderer::debug_renderer(const data::world& data) : data(data) {
+namespace {
+    
+    void printShaderInfoLog(GLuint obj) {
+        int infologLength = 0;
+        int charsWritten = 0;
+        char *infoLog;
+        ::glGetShaderiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+        if (infologLength > 0) {
+            infoLog = (char *)malloc(infologLength);
+            ::glGetShaderInfoLog(obj, infologLength, &charsWritten, infoLog);
+            std::cout << infoLog << std::endl;
+            free(infoLog);
+        }
+    }
+    
+    void printProgramInfoLog(GLuint obj) {
+        int infologLength = 0;
+        int charsWritten = 0;
+        char *infoLog;
+        ::glGetProgramiv(obj, GL_INFO_LOG_LENGTH, &infologLength);
+        if (infologLength > 0) {
+            infoLog = (char *)malloc(infologLength);
+            ::glGetProgramInfoLog(obj, infologLength, &charsWritten, infoLog);
+            std::cout << infoLog << std::endl;
+            free(infoLog);
+        }
+    }
+
+}
+
+rendering::debug_renderer::debug_renderer(const ::cgvkp::data::world& data) 
+    : data(data), 
+    vao(0), shader(0),
+    win_w(0), win_h(0),
+    win_scale(0.95f) {
 
 }
 
@@ -13,23 +49,139 @@ rendering::debug_renderer::~debug_renderer() {
 
 bool rendering::debug_renderer::init(const window& wnd) {
     wnd.make_current();
-    ::glClearColor(0.0f, 0.75f, 0.0f, 0.0f);
+    ::glClearColor(0.0f, 0.25f, 0.0f, 0.0f);
     ::glEnable(GL_DEPTH_TEST);
     ::glEnable(GL_CULL_FACE);
+
+    //
+    // Shaders
+    //
+    const char *vert_src = "#version 330 core\n\
+uniform vec2 area;\n\
+uniform vec2 p1;\n\
+uniform vec2 p2;\n\
+uniform float win_scale;\n\
+in float position;\n\
+\n\
+void main() {\n\
+    vec2 pos = mix(p1, p2, position);\n\
+    gl_Position = vec4( ((pos / area) * 2.0 - 1.0) * win_scale, 0.0, 1.0);\n\
+}";
+    const char *frag_src = "#version 330 core\n\
+uniform vec4 color;\n\
+out vec4 o_color;\n\
+void main() {\n\
+    o_color = color;\n\
+}";
+    GLuint v, f;
+    v = ::glCreateShader(GL_VERTEX_SHADER);
+    f = ::glCreateShader(GL_FRAGMENT_SHADER);
+    ::glShaderSource(v, 1, &vert_src, nullptr);
+    ::glCompileShader(v);
+    printShaderInfoLog(v);
+    ::glShaderSource(f, 1, &frag_src, nullptr);
+    ::glCompileShader(f);
+    printShaderInfoLog(f);
+
+    shader = ::glCreateProgram();
+    ::glAttachShader(shader, v);
+    ::glAttachShader(shader, f);
+    //::glBindFragDataLocation(p, 0, "output_color");
+    ::glLinkProgram(shader);
+    printProgramInfoLog(shader);
+
+    ::glUseProgram(shader);
+    ::glUniform2f(::glGetUniformLocation(shader, "area"), static_cast<float>(data.get_config().width()), static_cast<float>(data.get_config().height()));
+    ::glUniform1f(::glGetUniformLocation(shader, "win_scale"), win_scale);
+
+    //
+    // VAOs
+    //
+    const float dat[] = { 0.0f, 1.0 };
+    GLuint buf;
+
+    ::glGenVertexArrays(1, &vao);
+    ::glBindVertexArray(vao);
+    // Generate two slots for the vertex and color buffers
+    ::glGenBuffers(1, &buf);
+    //// bind buffer for vertices and copy data into buffer
+    ::glBindBuffer(GL_ARRAY_BUFFER, buf);
+    ::glBufferData(GL_ARRAY_BUFFER, sizeof(dat), dat, GL_STATIC_DRAW);
+    ::glEnableVertexAttribArray(0/*location*/);
+    ::glVertexAttribPointer(0, 1, GL_FLOAT, 0, 0, 0);
+
+    ::glBindVertexArray(0);
+    //::glDeleteBuffers(1, &buf); // because it already is bound to the bbox_vao
+
     return true;
 }
 
 void rendering::debug_renderer::render(const window& wnd) {
+    if (wnd.get_size(win_w, win_h)) {
+        ::glViewport(0, 0, win_w, win_h);
+    }
+    if ((win_w == 0) || (win_h == 0)) return;
 
-    //main_window->update_size(scene.camera());
-    //main_window->update_cursor(scene.cursor());
-    //manage_views_and_controllers();
-    //update_models();
     ::glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    //render_views();
+
+    ::glDisable(GL_DEPTH_TEST);
+    ::glDisable(GL_CULL_FACE);
+
+    ::glUseProgram(shader);
+    ::glBindVertexArray(vao);
+
+    const float marker_size = 0.5f * data.get_config().positional_epsilon();
+    const float marker_extra_size = 5.0f * marker_size;
+
+    // game area bounding box
+    set_color(1.0f, 1.0f, 1.0f);
+    draw_line(0.0f, 0.0f, data.get_config().width(), 0.0f);
+    draw_line(data.get_config().width(), 0.0f, data.get_config().width(), data.get_config().height());
+    draw_line(data.get_config().width(), data.get_config().height(), 0.0f, data.get_config().height());
+    draw_line(0.0f, data.get_config().height(), 0.0f, 0.0f);
+
+    // markers for hands
+    set_color(0.5f, 0.75f, 1.0f);
+    for (const data::world::hand_ptr& hand : data.get_hands()) {
+        draw_line(hand->x - marker_size, hand->y - marker_size, hand->x + marker_size, hand->y + marker_size);
+        draw_line(hand->x - marker_size, hand->y + marker_size, hand->x + marker_size, hand->y - marker_size);
+
+        draw_line(hand->x - 0.25f * marker_size, hand->y + marker_extra_size, hand->x + 0.25f * marker_size, hand->y + marker_extra_size);
+        draw_line(hand->x, hand->y, hand->x, hand->y + marker_extra_size * hand->height);
+    }
+
+    // markers for stars
+    set_color(1.0f, 0.9f, 0.0f);
+    for (const data::world::star_ptr& star : data.get_stars()) {
+        draw_line(star->x - marker_size, star->y - marker_size, star->x + marker_size, star->y + marker_size);
+        draw_line(star->x - marker_size, star->y + marker_size, star->x + marker_size, star->y - marker_size);
+
+        draw_line(star->x, star->y, star->x + marker_extra_size * star->dx, star->y + marker_extra_size * star->dy);
+    }
 
 }
 
 void rendering::debug_renderer::deinit() {
+    ::glBindVertexArray(0);
+    ::glDeleteVertexArrays(1, &vao);
+    ::glUseProgram(0);
+    ::glDeleteProgram(shader);
+}
 
+// some utility functions for simple und INEFFICIENT rendering
+
+void rendering::debug_renderer::set_color(float r, float g, float b, float a) {
+    ::glUniform4f(::glGetUniformLocation(shader, "color"), r, g, b, a);
+}
+
+void rendering::debug_renderer::set_p1(float x, float y) {
+    ::glUniform2f(::glGetUniformLocation(shader, "p1"), x, y);
+}
+
+void rendering::debug_renderer::set_p2(float x, float y) {
+    ::glUniform2f(::glGetUniformLocation(shader, "p2"), x, y);
+}
+
+void rendering::debug_renderer::draw_line() {
+    ::glDrawArrays(GL_LINES, 0, 2);
 }
--- src/rendering/debug_renderer.hpp
+++ src/rendering/debug_renderer.hpp
@@ -9,14 +9,31 @@ namespace rendering {
 
     class debug_renderer {
     public:
-        debug_renderer(const data::world& data);
+        debug_renderer(const ::cgvkp::data::world& data);
         ~debug_renderer();
 
         bool init(const window& wnd);
         void render(const window& wnd);
         void deinit();
     private:
-        const data::world& data;
+
+        // some utility functions for simple und INEFFICIENT rendering
+        inline void set_color(float r, float g, float b, float a = 1.0f);
+        inline void set_p1(float x, float y);
+        inline void set_p2(float x, float y);
+        inline void draw_line();
+        inline void draw_line(float x1, float y1, float x2, float y2) {
+            set_p1(x1, y1);
+            set_p2(x2, y2);
+            draw_line();
+        }
+
+        const ::cgvkp::data::world& data;
+
+        unsigned int vao;
+        unsigned int shader;
+        unsigned int win_w, win_h;
+        const float win_scale;
     };
 
 }
