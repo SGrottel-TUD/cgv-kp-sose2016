6c4730cc16e60a488ff43f573544536536ec7d88
fxwt <fwllrt@gmail.com>
2016-06-13 15:31:45 +0200

    Fix cloud classes

--- src/rendering/view/cloud_geometry.cpp
+++ src/rendering/view/cloud_geometry.cpp
@@ -1,6 +1,6 @@
 #include "rendering/view/cloud_geometry.hpp"
 #include "util/resource_file.hpp"
-#include "util/obj_parser.hpp"
+#include "util/objImporter.hpp"
 #include "rendering/technique.hpp"
 #include <iostream>
 #include <glm/glm.hpp>
@@ -10,44 +10,62 @@
 namespace cgvkp {
 	namespace rendering {
 		namespace view {
-			cloud_geometry::cloud_geometry() :
-				vao(0u), element_count(0u) {
+			cloud_geometry::cloud_geometry()
+				: vao(0), indexBuffer(0), indicesMode(GL_NONE), indicesCount(0), indicesType(GL_NONE)
+			{
+				for (int i = 0; i < numAttributes; ++i)
+				{
+					vertexBuffers[i] = 0;
+				}
 			}
 			cloud_geometry::~cloud_geometry() {
-				::glBindVertexArray(0u);
-				::glDeleteVertexArrays(1, &vao);
-				::glDeleteBuffers(2, buffers);
+				glBindVertexArray(0);
+				glDeleteVertexArrays(1, &vao);
+				glDeleteBuffers(numAttributes, vertexBuffers);
+				glDeleteBuffers(1, &indexBuffer);
 			}
 			bool cloud_geometry::init() {
+				util::ObjImporter mesh("meshes/sphere.obj", true);
+				if (!mesh.isValid())
+				{
+					return false;
+				}
 
-				::glGenVertexArrays(1, &vao);
-				::glBindVertexArray(vao);
+				glGenVertexArrays(1, &vao);
+				glBindVertexArray(vao);
 
-				// Set up geometry 
-				std::vector<float> poss;
-				std::vector<float> normals;
+				glGenBuffers(1, &vertexBuffers[position]);
+				glBindBuffer(GL_ARRAY_BUFFER, vertexBuffers[position]);
+				glBufferData(GL_ARRAY_BUFFER, mesh.getPositionsSize(), mesh.getPositions(), GL_STATIC_DRAW);
+				glEnableVertexAttribArray(position);
+				glVertexAttribPointer(position, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
-				util::obj_parser::parse("obj/sphere.obj", poss, normals);
+				if (mesh.hasNormals())
+				{
+					glGenBuffers(1, &vertexBuffers[normal]);
+					glBindBuffer(GL_ARRAY_BUFFER, vertexBuffers[normal]);
+					glBufferData(GL_ARRAY_BUFFER, mesh.getNormalsSize(), mesh.getNormals(), GL_STATIC_DRAW);
+					glEnableVertexAttribArray(normal);
+					glVertexAttribPointer(normal, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+				}
+				if (mesh.hasTextureCoords())
+				{
+					glGenBuffers(1, &vertexBuffers[textureCoord]);
+					glBindBuffer(GL_ARRAY_BUFFER, vertexBuffers[textureCoord]);
+					glBufferData(GL_ARRAY_BUFFER, mesh.getTextureCoordsSize(), mesh.getTextureCoords(), GL_STATIC_DRAW);
+					glEnableVertexAttribArray(textureCoord);
+					glVertexAttribPointer(textureCoord, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+				}
 
-				// Generate three buffers, vertex + normals
-				::glGenBuffers(2, buffers);
-				// bind buffer for vertices and copy data into buffer
-				::glBindBuffer(GL_ARRAY_BUFFER, buffers[0]);
-				::glBufferData(GL_ARRAY_BUFFER, poss.size() * sizeof(float), poss.data(), GL_STATIC_DRAW);
+				glGenBuffers(1, &indexBuffer);
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+				glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh.getIndicesSize(), mesh.getIndices(), GL_STATIC_DRAW);
 
-				::glEnableVertexAttribArray(IN_LOC_POSITION);
-				::glVertexAttribPointer(IN_LOC_POSITION, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+				glBindVertexArray(0);
 
-				::glBindBuffer(GL_ARRAY_BUFFER, buffers[1]);
-				::glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(float), normals.data(), GL_STATIC_DRAW);
-
-				::glEnableVertexAttribArray(IN_LOC_NORMAL);
-				::glVertexAttribPointer(IN_LOC_NORMAL, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-
-				::glBindVertexArray(0);
-
-				element_count = (unsigned int)poss.size();
-
+				indicesMode = mesh.getIndicesMode();
+				indicesCount = mesh.getIndicesCount();
+				indicesType = mesh.getIndicesType();
 				return true;
 			}
 		}
