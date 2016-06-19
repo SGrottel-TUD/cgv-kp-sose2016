c227c3830aa4032d5cee6c37de224935fa1382e0
Evilham <github@evilham.com>
2016-06-19 12:42:08 +0200

    Added a util::texture class.

---
+++ src/util/texture.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <GL/glew.h>
+
+namespace cgvkp {
+    namespace util {
+        class texture {
+        public:
+            // Dummy constructor, instances should be created with from_png / ...
+            texture();
+            virtual ~texture();
+
+
+            inline size_t width() { return _width; }
+            inline size_t height() { return _height; }
+            inline GLuint id() { return _id; }
+            inline bool uploaded() { return _uploaded; }
+            // Static members
+            static texture from_png(std::string const& filename,
+                GLint wrap_s = GL_REPEAT,
+                GLint wrap_t = GL_REPEAT,
+                GLint mag_filter = GL_LINEAR,
+                GLint min_filter = 0, // default: GL_LINEAR_MIPMAP_LINEAR (if genMipMaps == false: GL_LINEAR)
+                bool genMipMaps = true
+            );
+        private:
+            // Instance members
+            size_t _width, _height;
+            GLuint _id;
+            bool _has_alpha, _uploaded;
+            // Static members
+            static GLuint upload_and_get_id(
+                GLubyte **data,
+                GLsizei width,
+                GLsizei height,
+                GLint wrap_s = GL_REPEAT,
+                GLint wrap_t = GL_REPEAT,
+                GLint mag_filter = GL_LINEAR,
+                GLint min_filter = 0, // default: GL_LINEAR_MIPMAP_LINEAR (if genMipMaps == false: GL_LINEAR)
+                bool genMipMaps = true
+            );
+        };
+    }
+}
