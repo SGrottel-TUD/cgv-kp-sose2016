1fa13bff3c3814774963fbc85bf9cb39e74b624c
Brandon Schmitt <Brandon.Schmitt@live.de>
2016-06-23 14:40:12 +0200

    Generalized the Bezier Class for arbitrary degrees

--- src/util/bezier.hpp
+++ src/util/bezier.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <glm/glm.hpp>
 #include <vector>
 
 namespace cgvkp
@@ -13,12 +12,10 @@ namespace cgvkp
 		public:
 			Bezier(int degree, std::vector<T> const& curvePoints);
 			T sample(float t) const;
-			inline float getLength() { return length; }
+			inline float getLength() const { return length; }
 
 		private:
 			int binomial(int n, int k) const;
-			void addVirtPoints(std::vector<T> const& curvePoints);
-			void calcT();
 
 			int degree;
 			std::vector<T> curvePoints;
@@ -28,17 +25,58 @@ namespace cgvkp
 
 		template<typename T>
 		Bezier<T>::Bezier(int _degree, std::vector<T> const& inputPoints)
+			: length(0)
 		{
 			degree = _degree;
-			addVirtPoints(inputPoints);
-			calcT();
+
+			if (degree < 2)
+			{
+				throw std::exception("Degree must be greater than 1.");
+			}
+			if ((inputPoints.size() - 2) % (degree - 1))
+			{
+				throw std::exception("Only a multiple number of (degree - 1) control points are allowed.");
+			}
+
+			// Calculate virutal curve points if necessary.
+			curvePoints.push_back(inputPoints.front());
+			for (size_t i = degree; i < inputPoints.size() - 1; i += degree - 1)
+			{
+				for (int k = degree - 1; k > 0; --k)
+				{
+					curvePoints.push_back(inputPoints[i - k]);
+				}
+				curvePoints.push_back((inputPoints[i - 1] + inputPoints[i]) / 2.0f);
+			}
+			for (int i = degree - 1; i > 0; --i)
+			{
+				curvePoints.push_back(inputPoints[inputPoints.size() - 1 - i]);
+			}
+			curvePoints.push_back(inputPoints.back());
+
+			// Calculate the global ts for the segments.
+			segmentT.push_back(0);
+			for (size_t i = degree; i < curvePoints.size(); i += degree)
+			{
+				for (int k = 0; k < degree; ++k)
+				{
+					length += glm::length(curvePoints[i - k] - curvePoints[i - k - 1]);
+				}
+				segmentT.push_back(length);
+			}
+
+			length = segmentT.back();
+			for (auto& segT : segmentT)
+			{
+				segT /= length;
+			}
 		}
 
 		template<typename T>
 		T Bezier<T>::sample(float t) const
 		{
 			int i = 0;
-			for (; i < segmentT.size(); i++)
+			for (; i < segmentT.size(); ++i)
 			{
 				if (segmentT[i + 1] >= t)
 				{
@@ -47,67 +85,13 @@ namespace cgvkp
 			}
 			float tSeg = (t - segmentT[i]) / (segmentT[i + 1] - segmentT[i]);
 
-			T point;
+			T point(0);
 			for (int k = 0; k <= degree; ++k)
 			{
-				point += binomial(degree, k) * powf(tSeg, static_cast<float>(k)) * powf(1.0f - tSeg, static_cast<float>(degree - k)) * curvePoints[i * 2 + k];
+				point += binomial(degree, k) * powf(tSeg, static_cast<float>(k)) * powf(1.0f - tSeg, static_cast<float>(degree - k)) * curvePoints[i * degree + k];
 			}
 
 			return point;
-		}
-
-		template<typename T>
-		void Bezier<T>::addVirtPoints(std::vector<T> const& inputPoints)
-		{
-			if (degree == 2)
-			{
-				int countVirt = 0;
-				curvePoints.push_back(inputPoints[0]);
-				for (size_t i = 1; i < inputPoints.size() - 2; i++)
-				{
-					curvePoints.push_back(inputPoints[i]);
-					curvePoints.push_back((inputPoints[i] + inputPoints[i + 1]) / 2.0f);
-					++countVirt;
-				}
-				curvePoints.push_back(inputPoints[inputPoints.size() - 2]);
-				curvePoints.push_back(inputPoints.back());
-			}
-			else
-			{
-				curvePoints = inputPoints;
-			}
-		}
-
-		template<typename T>
-		void Bezier<T>::calcT()
-		{
-			segmentT.push_back(0);
-			length = 0;
-
-			if (degree == 2)
-			{
-				for (int i = 1; i < curvePoints.size(); i += 2)
-				{
-					length += glm::length(curvePoints[i] - curvePoints[i - 1]);
-					length += glm::length(curvePoints[i + 1] - curvePoints[i]);
-					segmentT.push_back(length);
-				}
-			}
-			else
-			{
-				for (int i = 1; i < curvePoints.size(); i += 2)
-				{
-					length += glm::length(curvePoints[i] - curvePoints[i - 1]);
-					length += glm::length(curvePoints[i + 1] - curvePoints[i]);
-				}
-			}
-
-			length = segmentT.back();
-
-			for (float& segT : segmentT)
-			{
-				segT /= segmentT.back();
-			}
 		}
 
 		template<typename T>
