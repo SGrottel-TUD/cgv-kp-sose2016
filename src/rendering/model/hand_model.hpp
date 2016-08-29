10576528648d1df98e87c9e46c390633eb7f5481
fxwt <fwllrt@gmail.com>
2016-08-29 16:06:19 +0200

    New hand model (now with 100% more stick)

--- src/rendering/model/hand_model.hpp
+++ src/rendering/model/hand_model.hpp
@@ -11,7 +11,7 @@ namespace model {
         hand_model() : graphic_model_base(
             glm::rotate(glm::radians(-90.0f), glm::vec3(1.0f, 0.0f, 0.0f)) *
             glm::rotate(glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f)) *
-            glm::scale(glm::vec3(0.50f))
+            glm::scale(glm::vec3(0.05f))
         ) {}
     };
 }
